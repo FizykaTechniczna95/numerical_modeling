8755ea7e630a6d747785f46cf61fcbe8bb1b00ba
aszadzinski <albert.szadzinski@protonmail.com>
2018-11-30 09:21:09 +0000

    3.2

--- alb/Pb/band_plot.py
+++ alb/Pb/band_plot.py
@@ -1,4 +1,9 @@
+import subprocess as sp
+import time as t
+from sys import argv
 
+for k in range(1,12):
+	template = """
 tasks
   0
   10
@@ -22,7 +27,7 @@ rgkmax
  7
 
 ngridk
-  11 11 11
+  {} {} {}
 
 rgkmax
   8.0
@@ -36,3 +41,16 @@ plot1d
   0.5   0.5   0.0
   0.5   0.25 -0.25
   0.5   0.0   0.0
+""".format(k,k,k)
+	with open('elk.in','w') as file:
+		file.writelines(template)
+	a =sp.check_output('../../../src/elk',shell=True)
+	print(a)	
+	a=sp.check_output('gracebat {}'.format(argv[2]),shell=True)
+	print(a)	
+	a=sp.check_output('convert {}.ps {}.png'.format(argv[2][:-4],argv[2][:-4]),shell=True)
+	print(a)	
+	a=sp.check_output('mv {}.png {}/BD{}{}{}.png'.format(argv[2][:-4],argv[1],k,k,k),shell=True)
+	print(a)	
+	#t.sleep(2)
+	print("--> {}".format(k))
